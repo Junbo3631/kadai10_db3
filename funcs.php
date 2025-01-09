97653dd75fb86911fd4fcc75712feb559a835d00
Junbo3631 <junbo.3631@gmail.com>
2025-01-10 03:22:57 +0900

    Update funcs.php

--- funcs.php
+++ funcs.php
@@ -26,7 +26,7 @@ function db_conn()
   try {
     $db_name = 'junbo3631_3size_db';    //データベース名
     $db_id   = 'junbo3631_3size_db';      //アカウント名
-    $db_pw   = 'junko3631';      //パスワード：MAMPは'root'
+    $db_pw   = ;      //パスワード：MAMPは'root'
     $db_host = 'mysql3104.db.sakura.ne.jp'; //DBホスト
     $pdo = new PDO('mysql:dbname=' . $db_name . ';charset=utf8;host=' . $db_host, $db_id, $db_pw);
     return $pdo;
